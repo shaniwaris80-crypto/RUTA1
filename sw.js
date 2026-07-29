bd42cec81f1d02831a8f02ef3e595a612d0e1bd8
shaniwaris80-crypto <shaniwaris80@gmail.com>
2026-07-29 17:12:04 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C='ruta-madrid-v11';const F=['./','./index.html','./manifest.webmanifest','./firebase-config.js','./seed-data.js'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
